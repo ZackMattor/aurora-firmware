51f199a2034682002c99f4180567bb5b9b6a390e
Zack Mattor <zmattor@gmail.com>
2021-02-21 00:29:18 +0000

    Adding an end byte to the full frame stream

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <Adafruit_NeoPixel.h>
 #include "QList.h"
 #include "config.h"
-#include "geometry/icosahedron.h"
 #include "ota_update.h"
+
+//#include "geometry/shard.h"
+#include "geometry/icosahedron.h"
 
 // WiFi/Network Settings
 const char* ssid        = CLIENT_SSID;
@@ -14,11 +16,11 @@ WiFiClient server_client;
 
 // Aurora/LED Variables
 String device_id;
-const String geometry = "icosahedron";
-Adafruit_NeoPixel *led_strip = new Adafruit_NeoPixel(GEOMETRY_WIDTH, NEOPIXEL_PIN, NEO_GRBW + NEO_KHZ800);
+const String geometry = GEOMETRY_TYPE;
+Adafruit_NeoPixel *led_strip = new Adafruit_NeoPixel(GEOMETRY_WIDTH, NEOPIXEL_PIN, LED_META);
 
 const unsigned int FRAME_BUFFER_SIZE = GEOMETRY_WIDTH * 3;
-byte frame_buffer[FRAME_BUFFER_SIZE];
+char frame_buffer[FRAME_BUFFER_SIZE];
 
 unsigned long loop_count = 0;
 unsigned long current_time = 0;
@@ -35,8 +37,9 @@ unsigned long next_fps_time = fps_interval;
 unsigned int frames = 0;
 
 void clear(unsigned long h, short int s, short int v) {
-  for(int x=0; x<20; x++) {
-    led_strip->setPixelColor(icosahedron_hardware_map[x], led_strip->ColorHSV(h,s,v));
+  for(int x=0; x<GEOMETRY_WIDTH; x++) {
+    //led_strip->setPixelColor(x, led_strip->ColorRGB(h,s,v));
+    led_strip->setPixelColor(hardware_map[x], 255,0,0);
   }
 
   led_strip->show();
@@ -73,6 +76,8 @@ void reconnect() {
 void setup() {
   delay(5000);
 
+  memset(frame_buffer, 0, FRAME_BUFFER_SIZE);
+
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
@@ -97,26 +102,62 @@ void setup() {
   reconnect();
 }
 
+short buffer_started = 0;
+short buffer_ready = 0;
+
 void loop() {
   if(WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("WiFi not connected...");
     ESP.restart();
   }
 
-  if(server_client.available() >= FRAME_BUFFER_SIZE) {
-    //Serial.println(server_client.available());
+  // 4444444440
+  // Transform data stream into frame buffer
+  while(server_client.available()) {
+    unsigned int buffer_length = strlen(frame_buffer);
+    char c = server_client.read();
 
-    for(short int i = 0; i < FRAME_BUFFER_SIZE; i++) {
-      char c = server_client.read();
+    // The end of the old and start of the new!
+    if(c == 0) {
+      buffer_started = 1;
 
-      frame_buffer[i] = c;
+      if(buffer_length != 0) {
+        buffer_ready = 1;
+        break;
+      }
+    } else {
+      if(buffer_started == 1 && buffer_length < FRAME_BUFFER_SIZE) {
+        // Push item onto frame buffer
+        strncat(frame_buffer, &c, 1);
+      }
     }
+  }
+
+  if(buffer_ready == 1) {
+    Serial.println("Buffer Ready");
 
     for(short int i = 0; i < FRAME_BUFFER_SIZE; i=i+3) {
-      led_strip->setPixelColor(icosahedron_hardware_map[i%20], led_strip->Color(frame_buffer[i], frame_buffer[i+1], frame_buffer[i+2]));
+      unsigned int led_id = i / 3;
+      //Serial.print(led_id);
+      //Serial.print("(");
+      //Serial.print((byte)frame_buffer[i]);
+      //Serial.print("|");
+      //Serial.print((byte)frame_buffer[i+1]);
+      //Serial.print("|");
+      //Serial.print((byte)frame_buffer[i+2]);
+      //Serial.print(") ");
+      if(led_id < 20) {
+        //led_strip->setPixelColor(led_id, led_strip->Color(frame_buffer[i], frame_buffer[i+1], frame_buffer[i+2]));
+        led_strip->setPixelColor(led_id, led_strip->Color(frame_buffer[i], frame_buffer[i+1], frame_buffer[i+2]));
+      }
     }
+
     frames++;
     led_strip->show();
+    //Serial.println("");
+
+    buffer_ready = 0;
+    memset(frame_buffer, 0, FRAME_BUFFER_SIZE);
   }
 
   ArduinoOTA.handle();
