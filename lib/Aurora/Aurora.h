54718b6770fbf1bddbe259fe2a678374bd71314d
Zack Mattor <zmattor@gmail.com>
2021-07-17 19:25:21 +0000

    Removing bad comments

--- lib/Aurora/Aurora.h
+++ lib/Aurora/Aurora.h
@@ -6,17 +6,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <LinkedList.h>
 
-//struct PwmChain {
-//  int pin;
-//};
-//
-//struct PwmChain {
-//  int pin;
-//};
-
-//LinkedList<Adafruit_NeoPixel> * neo_pixel_chains = new LinkedList<String>();
-//LinkedList<> * pwm_chains = new LinkedList<String>();
-
 void aurora_init(String endpoint, String id);
 void aurora_sendTelemetry(void);
 void aurora_connect(void);
